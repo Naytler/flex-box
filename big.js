e8f6665684416f79a9494573f476ed7f6d17bd31
Sagirov Ruslan <justrus93@mail.ru>
2021-11-08 23:34:19 +0300

    new js from sololearn

---
+++ big.js
@@ -0,0 +1,97 @@
+//document.write("hi world");
+//console.log("Hi fi");
+
+let x = 100;
+let y = 97;
+
+//document.write(x);
+
+
+
+/*if(x<y){
+    alert("yep");
+}else{
+    alert("not");
+}*/
+
+/*if(x == y){
+    document.write("=equal");
+}else if(x < y){
+    document.write("x < y");
+}else{
+    document.write("x > y");
+}*/
+
+
+/*
+let day = 1;
+
+switch(day){
+    case 1: document.write("monday");
+        break;   //после этой комынды происходит выход из блока
+    case 2: document.write("Tuesday");
+        break;
+    case 3: document.write("wednesday");
+        break;
+    default: document.write("another day");  // определяет код для запуска если нет соответсвий
+}
+*/
+/*
+for(peremennaiy = 1; peremennaiy <= 10; peremennaiy++){
+    document.write(peremennaiy + "<br/>");
+}
+
+let per = 1;
+for(; per <= 6; per++){
+    document.write(per)
+}
+
+for(i = 7; i <= 9; i++){
+    text = i;
+    document.write(i);
+}
+
+for(a = 1; a < 10; a++ ){
+    document.write(a);
+}*/
+
+/*
+let i = 0;
+while(i <= 9){
+    document.write(i + "<br/>");
+    i += 1;
+}
+
+let r = 10;
+
+do{
+    document.write(r+"<br/>");
+    r = r + 1;
+}
+while( r <= 15);*/
+
+/*
+for(i = 0;i <= 10; i++){
+    if(i == 5){
+        break;
+    }
+    document.write(i + "eb <br/>");
+}
+
+for(p = 0; p <= 10; p++){
+    if(p == 5){
+        continue;
+    }
+    document.write(p + "<br/>")
+}
+
+let sum = 0;
+for(u = 4; u < 8; u++){
+    if(u == 6){
+        continue;
+    }
+    sum = sum + u;
+}
+
+document.write(sum);
+*/
